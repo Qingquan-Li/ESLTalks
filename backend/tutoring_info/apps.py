3c1f290bb1a4e5ddc798a1e72441cb383ad64e24
Qingquan Li <liqqisme@gmail.com>
2023-04-29 12:08:18 -0400

    Create tutoring_info app replace meeting_info app

--- backend/tutoring_info/apps.py
+++ backend/tutoring_info/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MeetingInfoConfig(AppConfig):
+class TutoringInfoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'meeting_info'
+    name = 'tutoring_info'
